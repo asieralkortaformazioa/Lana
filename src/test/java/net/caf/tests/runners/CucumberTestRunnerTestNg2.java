f0c089e2c6c7d700591890a1c553b95524646816
a.alkorta <aalkorta.caf@gmail.com>
2014-10-09 16:30:07 +0200

    Pluto Login feature

--- src/test/java/net/caf/tests/runners/CucumberTestRunnerTestNg2.java
+++ src/test/java/net/caf/tests/runners/CucumberTestRunnerTestNg2.java
@@ -3,14 +3,13 @@ package net.caf.tests.runners;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-import java.io.IOException;
-
 /**
  * Created by developer on 10/9/14.
  */
 
 @CucumberOptions(
-		features = "features/rf02.08_GestionarErroresDeProyecto.feature",
+		//features = "features/rf02.08_GestionarErroresDeProyecto.feature.wip",
+		features = "features/rf00.00_Login.feature",
 		format = { "html:target/cucumber - html  - report", "json:target/cucumber - json - report.json" },
 		glue = { "net.caf.tests.stepdefs" }
 )
--- src/test/java/net/caf/tests/stepdefs/GeneralStepDefinitions.java
+++ src/test/java/net/caf/tests/stepdefs/GeneralStepDefinitions.java
@@ -2,21 +2,16 @@ package net.caf.tests.stepdefs;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
-import java.util.concurrent.TimeUnit;
-
 import net.caf.selenium.SeleniumSingleton;
+import net.caf.selenium.SeleniumUtils;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.internal.WrapsDriver;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.b2b2000.utils.config.lightwieght.ConfigManager;
-import com.b2b2000.utils.exceptions.B2BException;
-import com.thoughtworks.selenium.Selenium;
-import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium;
 
 import cucumber.api.PendingException;
 import cucumber.api.java.After;
@@ -30,31 +25,36 @@ import cucumber.api.java.es.Entonces;
  */
 public class GeneralStepDefinitions {
 
-
 	@Before
 	public void initSelenium() throws Exception {
 		System.out.println("Before General steps");
-//
-//		selenium = new WebDriverBackedSelenium(driver, "http://localhost:8080/pluto/portal");
-//		selenium.start();
-//		WebDriver driver = ((WrapsDriver) selenium).getWrappedDriver();
-//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-//		System.out.println("Getting google");
-		SeleniumSingleton.init();
-
-		SeleniumSingleton.getDriver().get("http://www.google.com");
-//		driver.get("http://www.google.com");
+		//
+		//		selenium = new WebDriverBackedSelenium(driver, "http://localhost:8080/pluto/portal");
 		//		selenium.start();
-
-
-
+		//		WebDriver driver = ((WrapsDriver) selenium).getWrappedDriver();
+		//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		//		System.out.println("Getting google");
+		SeleniumSingleton.init();
+		SeleniumSingleton.getDriver().get("http://www.google.com");
+		//		driver.get("http://www.google.com");
+		//		selenium.start();
 	}
 
 	@After
 	public void destroySelenium() {
 		SeleniumSingleton.stop();
-//		selenium.stop();
-//		driver.quit(); // quit is a webdriver method.
+		//		selenium.stop();
+		//		driver.quit(); // quit is a webdriver method.
+	}
+
+	@Dado("^estamos logados como \"(.*?)\" con password \"(.*?)\"$")
+	public void estamos_logados_como_con_password(String user, String pwd) throws Throwable {
+		//		estamos_en_la_pagina_nombre("Login");
+		String url = ConfigManager.getProperty("Login");
+		estamos_en_la_pagina_url(url);
+		dado_se_modifica_el_campo_por("j_username", user);
+		dado_se_modifica_el_campo_por("j_password", pwd);
+		se_pulsa("j_login");
 	}
 
 	@Dado("^estando en la página ((https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?)$")
@@ -64,20 +64,18 @@ public class GeneralStepDefinitions {
 		SeleniumSingleton.getSelenium().waitForPageToLoad(SeleniumSingleton.TIMEOUT_PAGE_LOAD);
 	}
 
-	@Dado("^estando en la página ([a-z|A-Z|0-9]*)$")
-	public void estamos_en_la_pagina_nombre(String nombrePagina) throws B2BException {
-		System.out.println("Url:" + nombrePagina);
-		String url = ConfigManager.getProperty(nombrePagina);
-		estamos_en_la_pagina_url(url);
-	}
-
-	@Dado("^estando en la página \"(.*?)\"$")
-	public void estando_en_la_página(String arg1) throws Throwable {
-		System.out.println("Url:" + arg1);
-		String url = ConfigManager.getProperty(arg1);
-		estamos_en_la_pagina_url(url);
-	}
-
+	//	@Dado("^estando en la página ([a-z|A-Z|0-9]*)$")
+	//	public void estamos_en_la_pagina_nombre(String nombrePagina) throws B2BException {
+	//		System.out.println("Url:" + nombrePagina);
+	//		String url = ConfigManager.getProperty(nombrePagina);
+	//		estamos_en_la_pagina_url(url);
+	//	}
+	//	@Dado("^estando en la página \"(.*?)\"$")
+	//	public void estando_en_la_página(String arg1) throws Throwable {
+	//		System.out.println("Url:" + arg1);
+	//		String url = ConfigManager.getProperty(arg1);
+	//		estamos_en_la_pagina_url(url);
+	//	}
 	@Dado("^se modifica el campo \"(.*?)\" por \"(.*?)\"$")
 	public void dado_se_modifica_el_campo_por(String arg1, String arg2) throws Throwable {
 		esperarA(arg1);
@@ -106,7 +104,8 @@ public class GeneralStepDefinitions {
 	//    }
 	public void se_modifica_el_campo_por_gen(String nombreCampo, String valorCampo) throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		//		throw new PendingException();
+		SeleniumUtils.typeFocusAndHighlight(SeleniumSingleton.getSelenium(), nombreCampo, valorCampo);
 	}
 
 	//    @Y("^en el campo (\"(.*?)\")$ se visualizará (\"(.*?)\")$")
@@ -117,29 +116,30 @@ public class GeneralStepDefinitions {
 	//    }
 	public void en_el_campo_se_visualizara_gen(String nombreCampo, String valorCampo) throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
-		//        throw new PendingException();
-		en_el_campo_se_visualizara_gen(nombreCampo, valorCampo);
+		throw new PendingException();
+		//		en_el_campo_se_visualizara_gen(nombreCampo, valorCampo);
 	}
 
 	@Dado("^estando logados como ([a-z|A-Z|0-9]*)$")
 	public void estando_logados_como(String username) throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
 		//throw new PendingException();
-
-
-
 	}
 
-	@Cuando("^vamos a la página (((https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?))$")
-	public void vamos_a_la_página_url() throws Throwable {
+	@Cuando("^(vamos a|estando en) la página (((https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?))$")
+	public void vamos_a_la_pagina_url(String m1, String url) throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		estamos_en_la_pagina_url(url);
+		//		throw new PendingException();
 	}
 
-	@Cuando("^vamos a la página ([a-z0-9_-]*)$")
-	public void vamos_a_la_página_nombre() throws Throwable {
+	@Cuando("^(vamos a|estando en) la página \"(.*?)\"$")
+	public void vamos_a_la_pagina_nombre(String m1, String name) throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		//		throw new PendingException();
+		System.out.println("Url:" + name);
+		String url = ConfigManager.getProperty(name);
+		estamos_en_la_pagina_url(url);
 	}
 
 	@Cuando("^se navega hasta (\"(.*?)\")$")
@@ -155,11 +155,23 @@ public class GeneralStepDefinitions {
 		throw new PendingException();
 	}
 
+	@Entonces("^\tse (tiene que|debería) ver el texto \"(.*?)\"$")
+	public void debería_ver_texto(String m1, String text) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		//		esperarA(arg1);
+		if (SeleniumUtils.checkTextPresent(SeleniumSingleton.getSelenium(), text)) {
+			throw new Exception("Text Not present " + text);
+		}
+	}
+
 	@Dado("^se pulsa \"(.*?)\"$")
 	public void se_pulsa(String arg1) throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
 		esperarA(arg1);
-		throw new PendingException();
+		SeleniumUtils.waitTime(3000);
+		SeleniumUtils.clickFocusAndHighlight(SeleniumSingleton.getSelenium(), arg1);
+		SeleniumSingleton.waitForPageToLoad();
+		//		throw new PendingException();
 	}
 
 	public void esperarA(String id) {
